91545da6b5186fd63678c28529f5812a9ace682b
Ben Cyna <benjamincyna@gmail.com>
2022-09-22 21:05:10 +0930

    just pushing my workspace incase you want to check. ben.cpp is my hackey solution and group.cpp is wrights nice solution currnetly set to .7

--- ben.cpp
+++ ben.cpp
@@ -1,15 +1,18 @@
 // a1766116, Cyna, Benjamin
-// a1768686, kumar, Sharath
+// a1768686, Kumar, Sharath
 // a1809221, Zhou, Minghan
 // a1808020, Zhang, Xingyuan 
 // group 4
+
 #include <iostream>
 #include <fstream>
 #include <deque>
 #include <vector>
+#include <functional>
+#include <queue>
 
 // std is a namespace: https://www.cplusplus.com/doc/oldtutorial/namespaces/
-const int TIME_ALLOWANCE = 8;  // allow to use up to this number of time slots at once
+int TIME_ALLOWANCE = 10;  // allow to use up to this number of time slots at once
 const int PRINT_LOG = 0; // print detailed execution trace
 
 class Customer
@@ -36,11 +39,13 @@ class Event
 public:
     int event_time;
     int customer_id;  // each event involes exactly one customer
+    int priority;
 
-    Event(int par_event_time, int par_customer_id)
+    Event(int par_event_time, int par_customer_id, int _priority)
     {
         event_time = par_event_time;
         customer_id = par_customer_id;
+        priority = _priority;
     }
 };
 
@@ -61,7 +66,7 @@ void initialize_system(
         customers.push_back(customer_from_file);
 
         // new customer arrival event
-        Event arrival_event(arrival_time, customer_id);
+        Event arrival_event(arrival_time, customer_id, priority);
         arrival_events.push_back(arrival_event);
 
         customer_id++;
@@ -73,7 +78,8 @@ void print_state(
     int current_time,
     int current_id,
     const std::deque<Event> &arrival_events,
-    const std::deque<int> &customer_queue)
+    const std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> &p0_queue,
+    const std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> &p1_queue)
 {
     out_file << current_time << " " << current_id << '\n';
     if (PRINT_LOG == 0)
@@ -86,10 +92,15 @@ void print_state(
         std::cout << "\t" << arrival_events[i].event_time << ", " << arrival_events[i].customer_id << ", ";
     }
     std::cout << '\n';
-    for (int i = 0; i < customer_queue.size(); i++)
-    {
-        std::cout << "\t" << customer_queue[i] << ", ";
-    }
+    // for (int i = 0; i < p0_queue.size(); i++)
+    // {
+    //     std::cout << "\t" << p0_queue[i] << ", ";
+    // }
+    // std::cout << '\n';
+    // for (int i = 0; i < p1_queue.size(); i++)
+    // {
+    //     std::cout << "\t" << p1_queue[i] << ", ";
+    // }
     std::cout << '\n';
 }
 
@@ -120,16 +131,22 @@ int main(int argc, char *argv[])
 
     int current_id = -1; // who is using the machine now, -1 means nobody
     int time_out = -1; // time when current customer will be preempted
-    std::deque<int> queue; // waiting queue
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> init_queue; // waiting queue
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> p0_queue; // waiting queue
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> p1_queue; // waiting queue
+    int priority_switch = 0;
+
 
     // step by step simulation of each time slot
     bool all_done = false;
     for (int current_time = 0; !all_done; current_time++)
     {
+        priority_switch++;
+
         // welcome newly arrived customers
         while (!arrival_events.empty() && (current_time == arrival_events[0].event_time))
         {
-            queue.push_back(arrival_events[0].customer_id);
+            init_queue.push({customers[arrival_events[0].customer_id].slots_remaining, arrival_events[0].customer_id});
             arrival_events.pop_front();
         }
         // check if we need to take a customer off the machine
@@ -140,20 +157,84 @@ int main(int argc, char *argv[])
                 int last_run = current_time - customers[current_id].playing_since;
                 customers[current_id].slots_remaining -= last_run;
                 if (customers[current_id].slots_remaining > 0)
-                    {
-                        // customer is not done yet, waiting for the next chance to play
-                        queue.push_back(current_id);
+                {
+                    // customer is not done yet, waiting for the next chance to play
+                    if(customers[current_id].priority == 0){
+                        p0_queue.push({customers[current_id].slots_remaining, current_id});
+                    }else{
+                        p1_queue.push({customers[current_id].slots_remaining, current_id});
                     }
+
+                }
                 current_id = -1; // the machine is free now
             }
         }
         // if machine is empty, schedule a new customer
         if (current_id == -1)
         {
-            if (!queue.empty()) // is anyone waiting?
+            if (!init_queue.empty()) // is anyone waiting?
             {
-                current_id = queue.front();
-                queue.pop_front();
+                //first = slots_remaining
+                //second  = current_id
+                auto a = init_queue.top();
+                current_id = a.second;
+                init_queue.pop();
+                TIME_ALLOWANCE = 3;
+                if (TIME_ALLOWANCE > customers[current_id].slots_remaining)
+                {
+                    time_out = current_time + customers[current_id].slots_remaining;
+                }
+                else
+                {
+                    time_out = current_time + TIME_ALLOWANCE;
+                }
+                customers[current_id].playing_since = current_time;
+            } else if (!p0_queue.empty() && (priority_switch % 4) != 0) // is anyone waiting?
+            {
+                //first = slots_remaining
+                //second  = current_id
+                auto a = p0_queue.top();
+                current_id = a.second;
+                p0_queue.pop();
+                TIME_ALLOWANCE = 12;
+
+                if (TIME_ALLOWANCE > customers[current_id].slots_remaining)
+                {
+                    time_out = current_time + customers[current_id].slots_remaining;
+                }
+                else
+                {
+                    time_out = current_time + TIME_ALLOWANCE;
+                }
+                customers[current_id].playing_since = current_time;
+            }else if(!p1_queue.empty()) // is anyone waiting?
+            {
+                //first = slots_remaining
+                //second  = current_id
+                auto a = p1_queue.top();
+                current_id = a.second;
+                p1_queue.pop();
+                TIME_ALLOWANCE = 12;
+
+                if (TIME_ALLOWANCE > customers[current_id].slots_remaining)
+                {
+                    time_out = current_time + customers[current_id].slots_remaining;
+                }
+                else
+                {
+                    time_out = current_time + TIME_ALLOWANCE;
+                }
+                customers[current_id].playing_since = current_time;
+            }
+            else if (!p0_queue.empty()) // is anyone waiting?
+            {
+                //first = slots_remaining
+                //second  = current_id
+                auto a = p0_queue.top();
+                current_id = a.second;
+                p0_queue.pop();
+                TIME_ALLOWANCE = 12;
+
                 if (TIME_ALLOWANCE > customers[current_id].slots_remaining)
                 {
                     time_out = current_time + customers[current_id].slots_remaining;
@@ -165,10 +246,10 @@ int main(int argc, char *argv[])
                 customers[current_id].playing_since = current_time;
             }
         }
-        print_state(out_file, current_time, current_id, arrival_events, queue);
+        print_state(out_file, current_time, current_id, arrival_events, p0_queue, p1_queue);
 
         // exit loop when there are no new arrivals, no waiting and no playing customers
-        all_done = (arrival_events.empty() && queue.empty() && (current_id == -1));
+        all_done = (arrival_events.empty() && p0_queue.empty() && p1_queue.empty() && (current_id == -1));
     }
 
     return 0;
